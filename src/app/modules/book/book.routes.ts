59c90bc863a0c2b5ccbcc28f1baef333afe11116
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-14 23:30:05 +0600

    update and clean up

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
-import { BookValidation } from "./book.validation";
 import { BookController } from "./book.controller";
+import { BookValidation } from "./book.validation";
 
 const router = express.Router();
 
