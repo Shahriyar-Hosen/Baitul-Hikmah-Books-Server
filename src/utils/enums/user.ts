eacf651c2eedb13550239d2ee97c99ad79c12074
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-16 21:20:08 +0600

    update utils

--- src/utils/enums/user.ts
+++ src/utils/enums/user.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 export enum ENUM_USER_ROLE {
-  ADMIN = 'admin',
-  BUYER = 'buyer',
-  SELLER = 'seller',
+  ADMIN = "admin",
+  BUYER = "buyer",
+  SELLER = "seller",
 }
