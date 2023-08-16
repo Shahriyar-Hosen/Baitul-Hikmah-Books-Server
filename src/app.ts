6820788db4bfaae6c3b1081868b4a57b923ce1f2
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-16 22:23:43 +0600

    clean up

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express, { Application, NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import { globalErrorHandle } from './app/middlewares/globalErrorHandle';
-import { routes } from './app/routes';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { globalErrorHandle } from "./app/middlewares/globalErrorHandle";
+import { routes } from "./app/routes";
 const app: Application = express();
 
 app.use(cookieParser());
@@ -11,16 +11,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 app.use(globalErrorHandle);
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: 'Not found',
+    message: "Not found",
     errorMessage: [
       {
         path: req.originalUrl,
-        message: 'Api not found',
+        message: "Api not found",
       },
     ],
   });
